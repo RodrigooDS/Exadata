11bcacab3774abd6ffaf761227ef0eea7a8bab29
Rodrigo <49086139+RodrigoDS12@users.noreply.github.com>
2019-04-24 00:07:28 -0400

    Update Home

--- Home.py
+++ Home.py
@@ -1,89 +1,90 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'EXADATA.ui'
-#
-# Created by: PyQt5 UI code generator 5.11.3
-#
-# WARNING! All changes made in this file will be lost!
-
-from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication
+from PyQt5.QtWidgets import QApplication, QMessageBox, QMainWindow, QAction, qApp
 from PyQt5 import QtCore, QtGui, QtWidgets
 from pprint import pprint
 from BD import Ui_MainBD
 from Muestra import Ui_MainMUESTRA
 from Cortarfecha import Ui_MainCortar
+from Influyentes import Ui_MainInfluyentes
 import logo_rc
 import sys
 
-class INDEX(object):
-    def setupUi(self, EXADATA):
-        #TITULO DE VENTANA
-        EXADATA.setObjectName("EXADATA")
-        #TAMAÑO DE VENTANA
-        EXADATA.resize(800, 600)
+
+class  Ui_MainWindow(QMainWindow):
+
+    def setupUI(self):
+        # TITULO DE VENTANA
+        self.setObjectName("EXADATA")
+        # TAMAÑO DE VENTANA
+        self.resize(800, 600)
 
         #LOGO
-        self.centralwidget = QtWidgets.QWidget(EXADATA)
+        self.centralwidget = QtWidgets.QWidget(self)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(165, 0, 435, 351))
         self.label.setStyleSheet("border-image: url(:/cct/logo.png);")
-        EXADATA.setCentralWidget(self.centralwidget)
+        self.setCentralWidget(self.centralwidget)
 
-        #CUADRO TEXTO
+        # CUADRO TEXTO
         self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
         self.textBrowser.setGeometry(QtCore.QRect(270, 285, 256, 192))
         self.textBrowser.setObjectName("textBrowser")
-        EXADATA.setCentralWidget(self.centralwidget)
+        self.setCentralWidget(self.centralwidget)
 
-        #BARRA MENU
-        self.menubar = QtWidgets.QMenuBar(EXADATA)
+        # BARRA MENU
+        self.menubar = QtWidgets.QMenuBar(self)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
         self.menubar.setObjectName("menubar")
 
-            #BOTON PROGRAMAS
+        # BOTON PROGRAMAS
         self.Programas = QtWidgets.QMenu(self.menubar)
-        self.BaseDeDatos = QtWidgets.QAction(EXADATA)
-        self.Muestra_Programa = QtWidgets.QAction(EXADATA)
-        self.Cortar_Fecha = QtWidgets.QAction(EXADATA)
+        self.BaseDeDatos = QtWidgets.QAction(self)
+        self.Muestra_Programa = QtWidgets.QAction(self)
+        self.Cortar_Fecha = QtWidgets.QAction(self)
+        self.Popularidad = QtWidgets.QAction(self)
+
         self.Programas.addAction(self.BaseDeDatos)
         self.Programas.addAction(self.Muestra_Programa)
         self.Programas.addAction(self.Cortar_Fecha)
+        self.Programas.addAction(self.Popularidad)
 
-            #BOTON AYUDA
+        # BOTON AYUDA
         self.Ayuda = QtWidgets.QMenu(self.menubar)
-        self.Sobre_que = QtWidgets.QAction(EXADATA)
+        self.Sobre_que = QtWidgets.QAction(self)
         self.Ayuda.addAction(self.Sobre_que)
 
-
-        EXADATA.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(EXADATA)
+        self.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(self)
         self.statusbar.setObjectName("statusbar")
-        EXADATA.setStatusBar(self.statusbar)
+        self.setStatusBar(self.statusbar)
         self.menubar.addAction(self.Programas.menuAction())
         self.menubar.addAction(self.Ayuda.menuAction())
-        #self.menubar.addAction(self.Ayuda.)
-        #FUNCIONES
-        self.BARRAMENU(EXADATA)
-        self.CUADROTEXTO(EXADATA)
-        QtCore.QMetaObject.connectSlotsByName(EXADATA)
+        # self.menubar.addAction(self.Ayuda.)
+        # FUNCIONES
+        self.BARRAMENU(self)
+        self.CUADROTEXTO(self)
+        QtCore.QMetaObject.connectSlotsByName(self)
 
     def BARRAMENU(self, EXADATA):
         _translate = QtCore.QCoreApplication.translate
-        EXADATA.setWindowTitle(_translate("EXADATA", "MainWindow"))
+        self.setWindowTitle(_translate("EXADATA", "MainWindow"))
 
         self.Programas.setTitle(_translate("EXADATA", "Programas"))
         self.BaseDeDatos.setText(_translate("EXADATA", "Base de dato"))
         self.Muestra_Programa.setText(_translate("EXADATA", "Muestra"))
         self.Cortar_Fecha.setText(_translate("EXADATA", "Cortar fecha"))
+        self.Popularidad.setText(_translate("EXADATA", "Popularidad"))
+
 
         self.Ayuda.setTitle(_translate("EXADATA", "Ayuda"))
         self.Sobre_que.setText(_translate("EXADATA", "Sobre que"))
 
-        #Funciones de menu
+        # Funciones de menu
         self.BaseDeDatos.triggered.connect(self.BASE)
         self.Muestra_Programa.triggered.connect(self.MUESTRA)
         self.Cortar_Fecha.triggered.connect(self.CORTAR)
+        self.Popularidad.triggered.connect(self.INFLUYENTES)
+
 
     def CUADROTEXTO(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -93,41 +94,56 @@ class INDEX(object):
                                             "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
                                             "p, li { white-space: pre-wrap; }\n"
                                             "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-                                            #PARRAFO 1
+                                            # PARRAFO 1
                                             "<p align=\"justify\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600; font-style:italic;\""
-                                                #TEXTO PARRAFO 1
+                                            # TEXTO PARRAFO 1
                                             ">ExaData es una aplicacion creada con la finalidad de facilitar el manejo de informacion que se encuentra en la red social Twitter.<""/span></p>\n"
-                                            
+
                                             "<p align=\"justify\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt; font-weight:600; color:#ffaa00;\"><br /></p>\n"
-                                            #PARRAFO 2
+                                            # PARRAFO 2
                                             "<p align=\"justify\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600; font-style:italic;\""
-                                                #TEXTO PARRAFO 2
+                                            # TEXTO PARRAFO 2
                                             ">$$$$$$$$</span></p></body></html>"))
 
     def BASE(self):
-        self.ventana=QtWidgets.QMainWindow()
-        self.ui=Ui_MainBD()
+        self.ventana = Ui_MainWindow()
+        self.ui = Ui_MainBD()
         self.ui.setupUi(self.ventana)
         self.ventana.show()
 
     def MUESTRA(self):
-        self.ventana=QtWidgets.QMainWindow()
-        self.ui=Ui_MainMUESTRA()
+        self.ventana = Ui_MainWindow()
+        self.ui = Ui_MainMUESTRA()
         self.ui.setupUi(self.ventana)
         self.ventana.show()
 
     def CORTAR(self):
-        self.ventana=QtWidgets.QMainWindow()
-        self.ui=Ui_MainCortar()
+        self.ventana = Ui_MainWindow()
+        self.ui = Ui_MainCortar()
+        self.ui.setupUi(self.ventana)
+        self.ventana.show()
+
+    def INFLUYENTES(self):
+        self.ventana = Ui_MainWindow()
+        self.ui = Ui_MainInfluyentes()
         self.ui.setupUi(self.ventana)
         self.ventana.show()
 
 
+    def closeEvent(self, event):
+        close = QMessageBox.question(self,
+                                     "Salir",
+                                     "Estas seguro que quieres salir?",
+                                      QMessageBox.Yes | QMessageBox.No)
+        if close == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
 
 if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    #TEMA
+    app = QApplication(sys.argv)
+
+    # TEMA
     app.setStyle('Windows')
     palette = QtGui.QPalette()
     palette.setColor(QtGui.QPalette.Window, QtGui.QColor(53, 53, 53))
@@ -144,9 +160,7 @@ if __name__ == "__main__":
     palette.setColor(QtGui.QPalette.HighlightedText, QtCore.Qt.black)
     app.setPalette(palette)
 
-    EXADATA = QtWidgets.QMainWindow()
-    ui = INDEX()
-    ui.setupUi(EXADATA)
-    EXADATA.show()
+    window = Ui_MainWindow()
+    window.setupUI()
+    window.show()
     sys.exit(app.exec_())
-
